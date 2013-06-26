dd361ddfecdac7a88a21140de44baf4473a24893
Carlos Rodriguez <carlos@s8f.org>
2013-06-26 13:38:28 -0700

    fix date test messed up by timezone of testing machine

--- test/date.js
+++ test/date.js
@@ -1,15 +1,13 @@
 describe('date', function () {
+  var date = new Date(Date.UTC(1969, 6, 21, 2, 56));
   it('create a bucket with a date', function () {
-    var date = new Date(1969, 6, 21, 2, 56);
     var ts = date.getTime();
     assert.equal(timebucket(date), 'ms' + ts);
   });
   it('create a bucket with a date and specific size', function () {
-    var date = new Date(1969, 6, 21, 2, 56);
-    assert.equal(timebucket('8h', date), '8h-491');
+    assert.equal(timebucket('8h', date), '8h-492');
   });
   it('backwards syntax', function () {
-    var date = new Date(1969, 6, 21, 2, 56);
-    assert.equal(timebucket(date, '8h'), '8h-491');
+    assert.equal(timebucket(date, '8h'), '8h-492');
   });
 });
